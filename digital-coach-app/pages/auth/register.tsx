91a850b34e92c56a46176e76263049895f2134cc
Robert Brandl <74463585+robertbrandl@users.noreply.github.com>
2024-02-22 19:17:59 -0500

    Update register.tsx

--- digital-coach-app/pages/auth/register.tsx
+++ digital-coach-app/pages/auth/register.tsx
@@ -49,7 +49,7 @@ export default function RegisterPage() {
       currentUser!.id
     );
 
-    await UserService.registerUser(currentUser!.id, {
+    UserService.registerUser(currentUser!.id, {
       name,
       concentration,
       proficiency,
