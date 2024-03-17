8973a19797ab0279344a372adc339fbde38cd10a
Robert Brandl <74463585+robertbrandl@users.noreply.github.com>
2024-03-17 15:02:41 -0400

    Update ThreadList.tsx

--- digital-coach-app/components/organisms/Forum/ThreadList.tsx
+++ digital-coach-app/components/organisms/Forum/ThreadList.tsx
@@ -56,6 +56,7 @@ function ThreadList({ threads, setLoading }) {
             <>
               <Card title={thread.title}>
                 <p>{thread.content}</p>
+                <p>thread.id</p>
                 <Button
                   variant='contained'
                   type='submit'
