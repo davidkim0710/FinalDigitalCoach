d1c20a10d64b1bd9f0e31f291dddff5b8c824f0e
Robert Brandl <74463585+robertbrandl@users.noreply.github.com>
2024-03-28 20:32:54 -0400

    Update browsequestions.tsx

--- digital-coach-app/pages/browsequestions.tsx
+++ digital-coach-app/pages/browsequestions.tsx
@@ -55,15 +55,7 @@ function BrowseQuestionsPage() {
     console.log("fired");
     async function fetchQuestions() {
       setLoading(true);
-      const questions = await QuestionService.getByFilters(
-      subjectSelect as TSubject,
-      typeSelect as TQuestionType,
-      experienceLevelSelect as TExperienceLevel,
-      popularityCheckbox,
-      searchText.toLowerCase().trim(),
-      RESULT_LIMIT,
-      lastVisible
-    );
+      const questions = await QuestionService.getPopularityDesc();
       console.log(questions);
       setLastVisible(questions.docs[questions.docs.length - 1]);
       setQuestionsData(questions.docs.map((doc) => doc.data()));
