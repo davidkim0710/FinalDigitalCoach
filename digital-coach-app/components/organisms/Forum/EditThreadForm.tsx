f5f06c7ba62ad3c25648793147ee0803d157ca23
Robert Brandl <74463585+robertbrandl@users.noreply.github.com>
2024-04-01 17:08:52 -0400

    Update EditThreadForm.tsx

--- digital-coach-app/components/organisms/Forum/EditThreadForm.tsx
+++ digital-coach-app/components/organisms/Forum/EditThreadForm.tsx
@@ -52,7 +52,7 @@ function EditThreadForm({ initialTitle, initialContent, onSubmit, onExit }) {
             variant='contained'
             color='error'
             onClick={onExit} // Call onClose function when the button is clicked
-            sx={{ marginRight: '30px'}}
+            sx={{ marginRight: '100px', marginLeft: '100px'}}
           >
             Exit
           </Button>
